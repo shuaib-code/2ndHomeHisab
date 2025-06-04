5a2ef03b6905fdf1370f67e98989190084980281
shuaib-code <shuaib.cyclist@gmail.com>
2025-06-04 09:22:57 +0600

    fix: kitten icon 😼

--- components/Brand.tsx
+++ components/Brand.tsx
@@ -1,10 +1,10 @@
-import { Heart } from "lucide-react";
+import { Cat } from "lucide-react";
 
 export function Brand() {
 	return (
 		<div className="flex items-center space-x-2 mb-4 mt-2">
 			<div className="bg-emerald-600 p-2 rounded-lg">
-				<Heart className="h-6 w-6 text-white" />
+				<Cat className="h-6 w-6 text-white" />
 			</div>
 			<div className="font-[family-name:var(--font-geist-sans)]">
 				<h3 className="text-xl font-bold text-white">2nd Home Hisab</h3>
